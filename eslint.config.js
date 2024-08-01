9f087285a40f6e185b6a8dbfe542e6d65d313c45
Joe Hildebrand <joe-github@cursive.net>
2024-08-01 00:21:25 -0500

    Update dependencies, fix lint issues, move to GHA publishing

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import cjs from "@cto.af/eslint-config/cjs.js";
+import mocha from "@cto.af/eslint-config/mocha.js"
+import mod from "@cto.af/eslint-config/module.js";
+
+export default [
+  {
+    ignores: [
+      "coverage/**",
+      "node_modules/**",
+      "**/*.d.ts",
+    ],
+  },
+  ...mod,
+  {
+    files: ["**/*.cjs"],
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  },
+  ...mocha,
+  {
+    files: [
+      "lib/widths.js",
+    ],
+    rules: {
+      "@stylistic/object-curly-spacing": "off",
+    },
+  },
+];
